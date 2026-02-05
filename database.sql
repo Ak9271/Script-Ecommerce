d64760d6ab33a5aeca2561ddfefc1b11d51c42aa
nakchaya <akchayan.nihalananth@ynov.com>
2026-02-05 22:25:24 +0100

    ajout creation db users

---
+++ database.sql
@@ -0,0 +1,11 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+    `id`INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL, 
+    `role` ENUM('user', 'admin') DEFAULT 'user',
+    `cree_le` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
