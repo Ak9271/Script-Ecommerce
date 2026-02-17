df88339e9ae7e8c2e5a6ddb46fae5ec4f11fdb21
lnorithp <norithpierre.lyquach@ynov.com>
2026-02-17 16:36:48 +0100

    suppression fichier inutile

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,6 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ScriptShop - Marketplace de Scripts</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <!-- Fixed path to be relative -->
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
